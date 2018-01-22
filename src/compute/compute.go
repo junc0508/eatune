dc18831d9827ee51f4603b9b26394d9ec8cf4524
JunichiIshida <JunichiIshida@192.168.0.139>
2018-01-22 22:58:30 +0900

    add

--- src/compute/compute.go
+++ src/compute/compute.go
@@ -9,12 +9,12 @@ func Calculate_history_data(history price.Show_candles) (bool, bool) {
 	var judg_act bool
 	if history.Candles[1].Volume > 5000 {
 		if history.Candles[1].Closebid-history.Candles[1].Openbid > 0 {
-			jugd_act = true
+			judg_act = true
 		} else {
-			jugd_act = false
+			judg_act = false
 		}
 		judg = true
 	}
-	return jugd, jugd_act
+	return judg, judg_act
 
 }
