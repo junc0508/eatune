bda313bdb86ac6c03db853f7d18f158c6f2e11cd
JunichiIshida <JunichiIshida@192.168.0.139>
2018-01-22 22:12:06 +0900

    add

--- src/price/rate.go
+++ src/price/rate.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+// instruments USD_JPY
 type Get_rate struct {
 	Instruments []Instruments_rate `json:"instruments"`
 }
@@ -15,6 +16,38 @@ type Instruments_rate struct {
 	Displayname   string `json:"displayName"`
 	Pip           string `json:"pip"`
 	Maxtradeunits int    `json:"maxTradeUnits"`
+}
+
+// Instrument End
+// get_price
+type Get_price struct {
+	Prices []Prices_rate `json:"prices"`
+}
+type Prices struct {
+	Instrument string `json:"instrument"`
+	Time       string `json:"time"`
+	Bid        int    `json:"bid"`
+	Ask        int    `json:"ask"`
+}
+
+// get_price end
+// get_candles
+type Get_candles struct {
+	Instument   string         `json:"instrument"`
+	Granularity string         `json:"granularity"`
+	Candles     []Candles_rate `json:"candles"`
+}
+type Candles_rate struct {
+	Time     string `json:"time"`
+	Openbid  int    `json:"openBid"`
+	Openask  int    `json:"openAsk"`
+	Highbid  int    `json:"highBid"`
+	Highask  int    `json:"highAsk"`
+	LowBid   int    `json:"lowbid"`
+	Closebid int    `json:"closeBid"`
+	Closeask int    `json:"closeAsk"`
+	Volume   int    `json:"volume"`
+	Complete bool   `json:"complete"`
 }
 
 func Get_current_rate(End_point string, Token string) []byte {
@@ -35,6 +68,31 @@ func Get_current_rate(End_point string, Token string) []byte {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return body
+
+}
+
+func Get_history_rate(End_point string, Token string) []byte {
+	//var
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", End_point+"/v1/candles?accountId=8307075&instruments=USD_JPY&granuarity=H4&count=2", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Bearer "+Token)
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return body
 
 }
