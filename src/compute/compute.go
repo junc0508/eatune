1b28f885326733dbf6e4855230dcbad893ed084c
JunichiIshida <JunichiIshida@192.168.0.139>
2018-01-22 23:04:26 +0900

    add

--- src/compute/compute.go
+++ src/compute/compute.go
@@ -4,11 +4,17 @@ import (
 	"github.com/junc0508/eatune/src/price"
 )
 
+const (
+	th_vol  = 500
+	ind     = 1
+	th_diff = 0
+)
+
 func Calculate_history_data(history price.Show_candles) (bool, bool) {
 	var judg bool
 	var judg_act bool
-	if history.Candles[1].Volume > 5000 {
-		if history.Candles[1].Closebid-history.Candles[1].Openbid > 0 {
+	if history.Candles[ind].Volume > th_vol {
+		if history.Candles[ind].Closebid-history.Candles[ind].Openbid > th_diff {
 			judg_act = true
 		} else {
 			judg_act = false
